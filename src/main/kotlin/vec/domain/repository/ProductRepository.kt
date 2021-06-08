6c8bc9d7a9786e1f57fa44328a51dc378551f2f1
tmyksj <33417830+tmyksj@users.noreply.github.com>
2021-06-09 06:57:41 +0900

    Refactor code

--- src/main/kotlin/vec/domain/repository/ProductRepository.kt
+++ src/main/kotlin/vec/domain/repository/ProductRepository.kt
@@ -1,11 +1,11 @@
 package vec.domain.repository
 
 import org.springframework.data.repository.reactive.ReactiveCrudRepository
-import org.springframework.stereotype.Repository
+import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.Transactional
 import vec.domain.entity.Product
 import java.util.*
 
-@Repository
+@Component
 @Transactional
 interface ProductRepository : ReactiveCrudRepository<Product, UUID>
