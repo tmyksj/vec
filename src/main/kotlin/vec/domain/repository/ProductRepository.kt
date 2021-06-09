c5695e8eaf713bd452a89f5c5f9f9fa06d6aec7a
tmyksj <33417830+tmyksj@users.noreply.github.com>
2021-06-09 22:14:14 +0900

    Fix ID type

--- src/main/kotlin/vec/domain/repository/ProductRepository.kt
+++ src/main/kotlin/vec/domain/repository/ProductRepository.kt
@@ -4,8 +4,7 @@ import org.springframework.data.repository.reactive.ReactiveCrudRepository
 import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.Transactional
 import vec.domain.entity.Product
-import java.util.*
 
 @Component
 @Transactional
-interface ProductRepository : ReactiveCrudRepository<Product, UUID>
+interface ProductRepository : ReactiveCrudRepository<Product, String>
