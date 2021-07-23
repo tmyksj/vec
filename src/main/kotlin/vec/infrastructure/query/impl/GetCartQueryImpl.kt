8284dd2c2d602ee16d99f1aca5b0166ccd3dfb6b
tmyksj <33417830+tmyksj@users.noreply.github.com>
2021-07-23 12:53:26 +0900

    Refactor code

--- src/main/kotlin/vec/infrastructure/query/impl/GetCartQueryImpl.kt
+++ src/main/kotlin/vec/infrastructure/query/impl/GetCartQueryImpl.kt
@@ -20,20 +20,19 @@ class GetCartQueryImpl(
         return Flux.defer {
             databaseClient.sql(
                 """
-                    select
-                        cart_product.id as cp_id,
-                        cart_product.product_id as cp_product_id,
-                        product.name as p_name,
-                        product.description as p_description,
-                        product.amount as p_amount,
-                        product.tax_rate as p_tax_rate,
-                        product.tax as p_tax,
-                        product.total as p_total,
-                        product.stock as p_stock,
-                        cart_product.added_date as cp_added_date
+                    select cart_product.id         as cp_id,
+                           cart_product.product_id as cp_product_id,
+                           product.name            as p_name,
+                           product.description     as p_description,
+                           product.amount          as p_amount,
+                           product.tax_rate        as p_tax_rate,
+                           product.tax             as p_tax,
+                           product.total           as p_total,
+                           product.stock           as p_stock,
+                           cart_product.added_date as cp_added_date
                     from cart
-                    left join cart_product on cart.id = cart_product.cart_id
-                    left join product on cart_product.product_id = product.id
+                             left join cart_product on cart.id = cart_product.cart_id
+                             left join product on cart_product.product_id = product.id
                     where cart.user_id = :id
                 """.trimIndent()
             ).bind("id", principal.id)
