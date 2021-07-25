85bc3842dd7e6595edf8eae505f8655a360d5211
tmyksj <33417830+tmyksj@users.noreply.github.com>
2021-07-25 20:25:11 +0900

    Add rendering component

--- src/main/kotlin/vec/presentation/controller/product/DetailController.kt
+++ src/main/kotlin/vec/presentation/controller/product/DetailController.kt
@@ -8,22 +8,25 @@ import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.reactive.result.view.Rendering
-import org.springframework.web.reactive.result.view.modelAttribute
 import org.springframework.web.server.ResponseStatusException
+import org.springframework.web.server.ServerWebExchange
 import org.springframework.web.server.ServerWebInputException
 import reactor.core.publisher.Mono
 import reactor.kotlin.core.publisher.onErrorMap
 import vec.domain.entity.User
+import vec.presentation.component.RenderingComponent
 import vec.presentation.form.product.DetailForm
 import vec.useCase.query.GetProductQuery
 
 @Controller
 class DetailController(
+    private val renderingComponent: RenderingComponent,
     private val getProductQuery: GetProductQuery,
 ) {
 
     @RequestMapping(method = [RequestMethod.GET], path = ["/product/{id}"])
     fun get(
+        serverWebExchange: ServerWebExchange,
         @AuthenticationPrincipal principal: User?,
         @Validated detailForm: DetailForm,
         bindingResult: BindingResult,
@@ -37,13 +40,13 @@ class DetailController(
                 principal = principal,
                 id = checkNotNull(detailForm.id),
             )
-        }.map {
-            Rendering.view("layout/default")
+        }.flatMap {
+            renderingComponent.view("layout/default")
                 .modelAttribute("principal", principal)
                 .modelAttribute("product", it)
                 .modelAttribute("template", "template/product/detail")
                 .status(HttpStatus.OK)
-                .build()
+                .build(serverWebExchange)
         }.onErrorMap(ServerWebInputException::class) {
             ResponseStatusException(HttpStatus.NOT_FOUND)
         }.onErrorMap(GetProductQuery.ProductIsNotFoundException::class) {
--- src/main/kotlin/vec/presentation/controller/signUp/IndexController.kt
+++ src/main/kotlin/vec/presentation/controller/signUp/IndexController.kt
@@ -8,53 +8,56 @@ import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.reactive.result.view.Rendering
-import org.springframework.web.reactive.result.view.modelAttribute
+import org.springframework.web.server.ServerWebExchange
 import org.springframework.web.server.ServerWebInputException
 import reactor.core.publisher.Mono
 import reactor.kotlin.core.publisher.onErrorResume
-import reactor.kotlin.core.publisher.toMono
 import vec.domain.entity.User
+import vec.presentation.component.RenderingComponent
 import vec.presentation.form.signUp.IndexForm
 import vec.useCase.command.RegisterUserCommand
 
 @Controller
 class IndexController(
+    private val renderingComponent: RenderingComponent,
     private val registerUserCommand: RegisterUserCommand,
 ) {
 
     @RequestMapping(method = [RequestMethod.GET], path = ["/sign-up"])
     fun get(
+        serverWebExchange: ServerWebExchange,
         @AuthenticationPrincipal principal: User?,
         indexForm: IndexForm,
     ): Mono<Rendering> {
         if (principal != null) {
-            return Mono.fromCallable {
-                Rendering.redirectTo("/")
+            return Mono.defer {
+                renderingComponent.redirect("/")
                     .status(HttpStatus.SEE_OTHER)
-                    .build()
+                    .build(serverWebExchange)
             }
         }
 
-        return Mono.fromCallable {
-            Rendering.view("layout/default")
+        return Mono.defer {
+            renderingComponent.view("layout/default")
                 .modelAttribute("principal", null)
                 .modelAttribute("template", "template/signUp/index")
                 .status(HttpStatus.OK)
-                .build()
+                .build(serverWebExchange)
         }
     }
 
     @RequestMapping(method = [RequestMethod.POST], path = ["/sign-up"])
     fun post(
+        serverWebExchange: ServerWebExchange,
         @AuthenticationPrincipal principal: User?,
         @Validated indexForm: IndexForm,
         bindingResult: BindingResult,
     ): Mono<Rendering> {
         if (principal != null) {
-            return Mono.fromCallable {
-                Rendering.redirectTo("/")
+            return Mono.defer {
+                renderingComponent.redirect("/")
                     .status(HttpStatus.SEE_OTHER)
-                    .build()
+                    .build(serverWebExchange)
             }
         }
 
@@ -75,26 +78,24 @@ class IndexController(
                 passwordRaw = checkNotNull(indexForm.password),
                 hasRoleConsumer = true,
             )
-        }.map {
-            Rendering.redirectTo("/sign-in")
+        }.flatMap {
+            renderingComponent.redirect("/sign-in")
                 .status(HttpStatus.SEE_OTHER)
-                .build()
+                .build(serverWebExchange)
         }.onErrorResume(ServerWebInputException::class) {
-            Rendering.view("layout/default")
+            renderingComponent.view("layout/default")
                 .modelAttribute("principal", null)
                 .modelAttribute("template", "template/signUp/index")
                 .status(HttpStatus.BAD_REQUEST)
-                .build()
-                .toMono()
+                .build(serverWebExchange)
         }.onErrorResume(RegisterUserCommand.EmailIsAlreadyInUseException::class) {
             bindingResult.rejectValue("email", "vec.presentation.form.signUp.IndexForm.email.alreadyInUse")
 
-            Rendering.view("layout/default")
+            renderingComponent.view("layout/default")
                 .modelAttribute("principal", null)
                 .modelAttribute("template", "template/signUp/index")
                 .status(HttpStatus.BAD_REQUEST)
-                .build()
-                .toMono()
+                .build(serverWebExchange)
         }
     }
 
