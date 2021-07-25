98c0a89cb33020bd3fe1647044e5d3ca0eca7998
tmyksj <33417830+tmyksj@users.noreply.github.com>
2021-07-26 06:56:16 +0900

    Fix to use rendering component

--- src/main/kotlin/vec/presentation/controller/order/IndexController.kt
+++ src/main/kotlin/vec/presentation/controller/order/IndexController.kt
@@ -6,60 +6,62 @@ import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.reactive.result.view.Rendering
+import org.springframework.web.server.ServerWebExchange
 import org.thymeleaf.spring5.context.webflux.ReactiveDataDriverContextVariable
 import reactor.core.publisher.Mono
 import reactor.kotlin.core.publisher.onErrorResume
-import reactor.kotlin.core.publisher.toMono
 import vec.domain.entity.User
+import vec.presentation.component.RenderingComponent
 import vec.useCase.command.OrderCommand
 import vec.useCase.query.GetOrderListQuery
 
 @Controller
 class IndexController(
+    private val renderingComponent: RenderingComponent,
     private val orderCommand: OrderCommand,
     private val getOrderListQuery: GetOrderListQuery,
 ) {
 
     @RequestMapping(method = [RequestMethod.GET], path = ["/order"])
     fun get(
+        serverWebExchange: ServerWebExchange,
         @AuthenticationPrincipal principal: User,
     ): Mono<Rendering> {
         return Mono.fromCallable {
             getOrderListQuery.perform(
                 principal = principal,
             )
-        }.map {
-            Rendering.view("layout/default")
+        }.flatMap {
+            renderingComponent.view("layout/default")
                 .modelAttribute("orderList", ReactiveDataDriverContextVariable(it))
                 .modelAttribute("principal", principal)
                 .modelAttribute("template", "template/order/index")
                 .status(HttpStatus.OK)
-                .build()
+                .build(serverWebExchange)
         }
     }
 
     @RequestMapping(method = [RequestMethod.POST], path = ["/order"])
     fun post(
+        serverWebExchange: ServerWebExchange,
         @AuthenticationPrincipal principal: User,
     ): Mono<Rendering> {
         return Mono.defer {
             orderCommand.perform(
                 principal = principal,
             )
-        }.map {
-            Rendering.redirectTo("/")
+        }.flatMap {
+            renderingComponent.redirect("/")
                 .status(HttpStatus.SEE_OTHER)
-                .build()
+                .build(serverWebExchange)
         }.onErrorResume(OrderCommand.CartIsEmptyException::class) {
-            Rendering.redirectTo("/cart")
+            renderingComponent.redirect("/cart")
                 .status(HttpStatus.SEE_OTHER)
-                .build()
-                .toMono()
+                .build(serverWebExchange)
         }.onErrorResume(OrderCommand.ProductIsNotFoundException::class) {
-            Rendering.redirectTo("/cart")
+            renderingComponent.redirect("/cart")
                 .status(HttpStatus.SEE_OTHER)
-                .build()
-                .toMono()
+                .build(serverWebExchange)
         }
     }
 
