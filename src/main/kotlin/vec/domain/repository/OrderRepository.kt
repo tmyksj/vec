a447121504c5a8a078cffe7ab55786404cda00a2
tmyksj <33417830+tmyksj@users.noreply.github.com>
2021-07-31 11:07:36 +0900

    Add docs

--- src/main/kotlin/vec/domain/repository/OrderRepository.kt
+++ src/main/kotlin/vec/domain/repository/OrderRepository.kt
@@ -8,6 +8,11 @@ import vec.domain.entity.Order
 @NoRepositoryBean
 interface OrderRepository : ReactiveCrudRepository<Order, String> {
 
+    /**
+     * ユーザの注文を返します。
+     * @param userId ユーザ ID
+     * @return 注文
+     */
     fun findAllByUserId(userId: String): Flux<Order>
 
 }
