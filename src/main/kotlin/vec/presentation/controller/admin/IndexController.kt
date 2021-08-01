88c7d138ac79315f3e10adeec7f057d98269a43d
tmyksj <33417830+tmyksj@users.noreply.github.com>
2021-08-01 20:00:08 +0900

    Add admin page

---
+++ src/main/kotlin/vec/presentation/controller/admin/IndexController.kt
@@ -0,0 +1,33 @@
+package vec.presentation.controller.admin
+
+import org.springframework.http.HttpStatus
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.reactive.result.view.Rendering
+import org.springframework.web.server.ServerWebExchange
+import reactor.core.publisher.Mono
+import vec.domain.entity.User
+import vec.presentation.component.RenderingComponent
+
+@Controller
+class IndexController(
+    private val renderingComponent: RenderingComponent,
+) {
+
+    @RequestMapping(method = [RequestMethod.GET], path = ["/admin"])
+    fun get(
+        serverWebExchange: ServerWebExchange,
+        @AuthenticationPrincipal principal: User,
+    ): Mono<Rendering> {
+        return Mono.defer {
+            renderingComponent.view("layout/default")
+                .modelAttribute("principal", principal)
+                .modelAttribute("template", "template/admin/index")
+                .status(HttpStatus.OK)
+                .build(serverWebExchange)
+        }
+    }
+
+}
